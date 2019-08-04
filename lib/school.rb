9380122ee24d079e8bcb74c3eaa69cb3e09d8ff0
Adam Schonely <aschonely@gmail.com>
2019-08-04 01:09:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_accessor :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  
+  def sort
+  
+  end
+end  
